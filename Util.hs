4b96d6e048df5ef892b3d7c0c566f5a5c8e45aba
Jared Updike <jupdike@gmail.com>
2013-07-17 15:35:07 -0700

    Undid some unnecessary changes to get things to compile

--- Util.hs
+++ Util.hs
@@ -8,9 +8,9 @@ import Data.List(sort, sortBy, group, groupBy, union, partition)
 import Libs.Trace
 import Libs.IOUtil (progArgs)
 
-doTrace = False --elem "-debug" progArgs
-doTrace2 = False -- length (filter (== "-debug") progArgs) > 1
-doFull = False -- elem "-full" progArgs
+doTrace = elem "-debug" progArgs
+doTrace2 = length (filter (== "-debug") progArgs) > 1
+doFull = elem "-full" progArgs
 
 tracex s x = if doTrace then traces s x else x
 tracex2 s x = if doTrace2 then traces s x else x
